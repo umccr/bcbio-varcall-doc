4ea68c68af04f91e2df5bf3594be1b136cafe27d
pdiakumis <pdiakumis@unimelb.edu.au>
2017-09-19 10:59:50 +1000

    starting VarScan

---
+++ scripts/bcbio/2017-09-19_cancer-normal/work/run_bcbio.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#SBATCH -n 1
+#SBATCH -p vccc
+#SBATCH --mem=8000
+#SBATCH --time=240:00:00
+#SBATCH -o log.nortum_varscan.out
+#SBATCH -e log.nortum_varscan.err
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=pdiakumis@unimelb.edu.au
+
+bcbio_nextgen.py \
+    ../config/cancer-normal.yaml \
+    -n 64 \
+    -q vccc \
+    -s slurm \
+    -t ipython \
+    -r 'timelimit=5-00:00:00' \
+    --retries 1 \
+    --timeout 120
--- scripts/bcbio/README.md
+++ scripts/bcbio/README.md
@@ -3,3 +3,5 @@
 * `2017-09-11_cancer-normal`: bcbio run with MuTect2.
     * Needed to add `tools_on: [gatk4]` in the config file.
     * Needed to add `tools_off: [gemini]` in the config file.
+* `2017-09-19_cancer-normal`: bcbio run with VarScan.
+    * Removed `tools_on: [gatk4]` in the config file.
